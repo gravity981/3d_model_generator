80ab8c0ce4284dfabc6bff8c3f18adc5b7857ef2
Nicolas Marty <nicolas.marty@gmx.net>
2022-02-22 12:06:09 +0100

    simplify generate script usage

--- scripts/generate_all_tokens.sh
+++ scripts/generate_all_tokens.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-python3 "$PWD/src/model_generator.py" \
+xvfb-run -a python3 "$PWD/src/model_generator.py" \
   --scad-file "$PWD/models/token_3d.scad" \
   --conf-file "$PWD/config/all_tokens.json" \
   --output-dir "$PWD/output" \
   --thumbnails
+
+montage -tile 11x0 -geometry +0+0 "$PWD/output/thumbnail/*.png" "$PWD/output/poster.png"
--- scripts/stitch_thumbnails.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-montage -tile 11x0 -geometry +0+0 "$PWD/output/thumbnail/*.png" "$PWD/output/poster.png"
