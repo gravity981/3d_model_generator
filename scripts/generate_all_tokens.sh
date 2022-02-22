3ea6f17b6fdcb8808109e73bf2064a6e3405cdbc
Nicolas Marty <nicolas.marty@gmx.net>
2022-02-22 17:44:25 +0100

    update script output paths

--- scripts/generate_all_tokens.sh
+++ scripts/generate_all_tokens.sh
@@ -3,4 +3,4 @@
 sudo docker run -v $PWD:/work gravity981/3dgen \
   -m models \
   -c config/all_tokens.json \
-  -o output
+  -o output/all_tokens
--- scripts/generate_default_token.sh
+++ scripts/generate_default_token.sh
@@ -3,4 +3,4 @@
 sudo docker run -v $PWD:/work gravity981/3dgen \
   -m models \
   -c config/default_token.json \
-  -o output
+  -o output/default_token
